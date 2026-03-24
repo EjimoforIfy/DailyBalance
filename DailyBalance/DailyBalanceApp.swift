189bf8ee92e7201825015abfa6e0058313d41318
30108586 <30108585@students.southwales.ac.uk>
2026-03-24 14:50:44 +0000

    Initial Commit

---
+++ DailyBalance/DailyBalanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyBalanceApp.swift
+//  DailyBalance
+//
+//  Created by Ejimofor I J (FCES) on 24/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct DailyBalanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
